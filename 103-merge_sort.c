4c6430c60fde6476ae990de176599015f90b1738
Samstiles-Dev <ayosamuel777@gmail.com>
2023-11-25 11:50:56 -0800

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,35 +3,55 @@
 #include <stdio.h>
 
 /**
+ * merge_sort - initiate merge sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *temp;
+
+	if (!array || size < 2)
+		return;
+
+	temp = malloc(sizeof(*temp) * size);
+	if (!temp)
+		return;
+
+	_merge_sort(array, temp, size);
+	free(temp);
+}
+
+/**
  * _merge_sort - start merge sort
  * @array: array to be sorted
- * @temp: temp array for holding sorted elements
+ * @temp: temporary array for holding sorted elements
  * @size: size of the array
  */
 void _merge_sort(int *array, int *temp, size_t size)
 {
-	size_t _half = size / 2, i = 0, j = 0, k;
+	size_t half = size / 2, i = 0, j = 0, k;
 
 	if (size < 2)
 		return;
 
-	_merge_sort(array, temp, _half);
-	_merge_sort(array + _half, temp + _half, size - _half);
+	_merge_sort(array, temp, half);
+	_merge_sort(array + half, temp + half, size - half);
 
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(array, _half);
+	print_array(array, half);
 	printf("[right]: ");
-	print_array(array + _half, size - _half);
+	print_array(array + half, size - half);
 	for (k = 0; k < size; k++)
-		if (j >= size - _half || (i < _half && array[i] < (array + _half)[j]))
+		if (j >= size - half || (i < half && array[i] < (array + half)[j]))
 		{
 			temp[k] = array[i];
 			i++;
 		}
 		else
 		{
-			temp[k] = (array + _half)[j];
+			temp[k] = (array + half)[j];
 			j++;
 		}
 	for (k = 0; k < size; k++)
@@ -40,22 +60,4 @@ void _merge_sort(int *array, int *temp, size_t size)
 	print_array(array, size);
 }
 
-/**
- * merge_sort - start merge sort
- * @array: array to be sorted
- * @size: size of the array
- */
-void merge_sort(int *array, size_t size)
-{
-	int *_temp;
 
-	if (!array || size < 2)
-		return;
-
-	_temp = malloc(sizeof(*_temp) * size);
-	if (!temp)
-		return;
-
-	_merge_sort(array, _temp, size);
-	free(_temp);
-}
